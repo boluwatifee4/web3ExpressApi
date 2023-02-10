c195e4798b1f9365be793e572458af17d6e6e768
boluwatifee1 <boluwatifee4@gmail.com>
2023-02-10 18:23:29 +0100

    updates

--- api/product.js
+++ api/product.js
@@ -8,13 +8,11 @@ const router = express.Router();
  *
  * @return product list | empty.
  */
-const MORALIS_API_KEY = 'J0z4vfkCmqGFlqy7RzkQsMWRlsUcR5Ek3Ftl1AbMbjx9cBFHIfq9uvfyVOVNtsRe';
+
+let MORALIS_APIB_KEY = 'J0z4vfkCmqGFlqy7RzkQsMWRlsUcR5Ek3Ftl1AbMbjx9cBFHIfq9uvfyVOVNtsRe';
 const address = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d';
 
-var allowedOrigins = ['http://localhost:4200',
-                      'http://yourapp.com'];
-
-
+let apiKey$ = "J0z4vfkCmqGFlqy7RzkQsMWRlsUcR5Ek3Ftl1AbMbjx9cBFHIfq9uvfyVOVNtsRe";
 
 router.get("/", async (req, res) => {
   try {
@@ -28,24 +26,281 @@ router.get("/", async (req, res) => {
   }
 });
 
+
 router.get('/resolveUd/:domain', async (req, res) => {
+
+  // startServer(MORALIS_APIB_KEY);
   try {
+    const { domain } = req.params;
+    const result = await Moralis.EvmApi.resolve.resolveDomain({
+      domain,
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.json({ error: error.message });
+  }
+});
+
+
+router.get('/resolveUdDomains/:domain', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    //  await startServer(MORALIS_API_KEY);
+    try {
       const { domain } = req.params;
       const result = await Moralis.EvmApi.resolve.resolveDomain({
-          domain,
+        domain,
       });
       res.status(200).json(result);
-  } catch (error) {
+    } catch (error) {
       console.error(error);
       res.status(500);
       res.json({ error: error.message });
+    }
   }
 });
-const startServer = async () => {
-await Moralis.start({
-  apiKey: MORALIS_API_KEY,
+
+
+router.get('/resolveEns/:address', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  apiKey$ = MORALIS_API_KEY;
+  // console.log(apiKey$)
+  if (!MORALIS_API_KEY) {
+
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    // reStartSever();
+    try {
+      const { address } = req.params;
+      const result = await Moralis.EvmApi.resolve.resolveAddress({
+        address,
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+})
+
+
+// TOKEN APIS
+
+// get all ERC20 tokens owned by an address
+
+router.get('/getERC20Tokens/:address', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+      const { address } = req.params;
+      const chain = EvmChain.ETHEREUM;
+      console.log(address);
+
+      const result = await Moralis.EvmApi.token.getWalletTokenBalances({
+        chain,
+        address,
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
 });
+
+// get all ERC20 transfers by contract
+
+router.get('/getERC20Transfers/:contract', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+      const { contract } = req.params;
+      const chain = EvmChain.ETHEREUM;
+
+      const result = await Moralis.EvmApi.token.getTokenTransfers({
+        chain,
+        address,
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+});
+
+// get all ERC20 transfers by wallet
+
+router.get('/getERC20TransfersByWallet/:address', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+      const { address } = req.params;
+      const chain = EvmChain.ETHEREUM;
+
+      const result = await Moralis.EvmApi.token.getWalletTokenTransfers({
+        chain,
+        address,
+      });
+
+      res.status(200).json(result); 
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+});
+
+// get ERC20 metadata by contract
+
+router.get('/getERC20Metadata/:contract', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+      const { contract } = req.params;
+      const chain = EvmChain.ETHEREUM;
+      
+      const result = await Moralis.EvmApi.token.getTokenMetadata({
+        chain,
+        address,
+      });
+
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+});
+
+
+
+// Balance api
+
+// get the native balance of an address
+
+router.get('/getNativeBalance/:address', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+      const { address } = req.params;
+      const chain = EvmChain.ETHEREUM;
+
+      const result = await Moralis.EvmApi.balance.getNativeBalance({
+        chain,
+        address,
+      });
+
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+});
+
+// get the native balance of an ERC20 token vault
+
+router.get('/getERC20VaultBalance/:address', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+      const { address } = req.params;
+      const chain = EvmChain.ETHEREUM;
+
+      const result = await Moralis.EvmApi.balance.getERC20Balance({
+        chain,
+        address,
+      });
+
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+});
+
+// get the native balance of a multi-signature wallet
+
+router.get('/getMultiSigBalance/:address', async (req, res) => {
+  const MORALIS_API_KEY = req.headers['x-api-key'];
+  if (!MORALIS_API_KEY) {
+    res.status(500);
+    res.json({ error: 'please add an x-api-key request header when sending requests' });
+  } else {
+    // await startServer(MORALIS_API_KEY);
+    try {
+
+      const { address } = req.params;
+      const chain = EvmChain.ETHEREUM;
+
+      const result = await Moralis.EvmApi.balance.getNativeBalance({
+        chain,
+        address,
+      });
+
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+      res.json({ error: error.message });
+    }
+  }
+});
+
+
+
+
+const startServer = async () => {
+  await Moralis.start({
+    apiKey: apiKey$,
+  });
+
 };
-startServer();
+
+const reStartSever = async () => {
+    startServer();
+}
+
+reStartSever();
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const product = require("./api/product");
 var cors = require('cors');
-
+const bodyParser = require('body-parser');
 app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+app.use(express.json({ extended: true }));
 
-app.use(express.json({ extended: false }));
-
-app.use("/api/product", product);
+app.use("/v1", product);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
